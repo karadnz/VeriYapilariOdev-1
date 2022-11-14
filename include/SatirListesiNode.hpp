ccfd47163e483a748d88011ac3e41c4439d9a1b1
Mustafa Karadeniz <mkaraden@student.42istanbul.com.tr>
2022-11-14 15:34:38 +0300

    updated

---
+++ include/SatirListesiNode.hpp
@@ -0,0 +1,16 @@
+/**
+* @file SatirListediNode.hpp
+* @description header
+* @course 2A
+* @assignment 1
+* @date Kodu oluşturduğunuz Tarih
+* @author Mustafa Karadeniz
+*/
+
+#ifndef SATIRLISTESINODE_HPP
+#define SATIRLISTEDINODE_HPP
+
+#include <iostream>
+using namespace std;
+
+#endif
